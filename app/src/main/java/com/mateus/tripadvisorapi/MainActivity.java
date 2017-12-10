9b8a75869f0a5463831c4ef18cbf0322b1fe8381
Mateus Barreto <mateus_k2k@hotmail.com>
2017-12-10 19:13:59 -0300

    Added location functionality

--- app/src/main/java/com/mateus/tripadvisorapi/MainActivity.java
+++ app/src/main/java/com/mateus/tripadvisorapi/MainActivity.java
@@ -1,9 +1,18 @@
 package com.mateus.tripadvisorapi;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,9 +23,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Button gpsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = locListener;
+
+        gpsButton = (Button) findViewById(R.id.gpsButton);
+        gpsButton.setOnClickListener(gpsButtonClickListener);
     }
 
     @Override
@@ -87,4 +107,55 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private View.OnClickListener gpsButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            enableLocation();
+        }
+    };
+
+    private void enableLocation () {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+            Log.i("DEBUG", "REQUEST PERMISSIONS");
+            return;
+        } else {
+            Log.i("DEBUG", "PERMISSIONS GRANTED");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                enableLocation();
+            }
+        }
+    }
+
+
+    private LocationListener  locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i("LOCATION", location.getLatitude() + " " + location.getLongitude());
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 }
